0b723427e4f0332b68e3789689401f8b8a6102a5
eyrim <marfxedbot@gmail.com>
2024-12-24 00:04:54 +0000

    Add wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,27 +5,11 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
-local function get_cursor_theme()
-	local success, stdout, stderr =
-		wezterm.run_child_process { 'xprop', '-root' }
-	if not success then
-		wezterm.log_error(
-			('Command exited with non-zero exit code.\nStdout:\n%s\nStderr:\n%s'):format(
-				stdout,
-				stderr
-			)
-		)
-		return nil
-	end
-
-	return stdout:match [[RESOURCE_MANAGER%(STRING%) = ".+\nXcursor.theme:\t(.+)\n.+"]]
-end
-
--- config.xcursor_theme = get_cursor_theme()
 
 -- For example, changing the color scheme:
-config.color_scheme = "AdventureTime"
+config.color_scheme = 'AdventureTime'
+config.front_end = "WebGpu"
+config.enable_wayland = false
 
 -- and finally, return the configuration to wezterm
 return config
-
