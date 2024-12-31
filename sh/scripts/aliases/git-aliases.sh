cf681b4981dac5d7d6398437e1262131c291eb92
eyrim <marfxedbot@gmail.com>
2024-12-31 01:10:22 +0000

    Add git add alias

--- sh/scripts/aliases/git-aliases.sh
+++ sh/scripts/aliases/git-aliases.sh
@@ -5,3 +5,7 @@ gs() {
 gp() {
     git pull;
 }
+
+ga() {
+        git status -s | fzf --sync --preview="echo {} | awk '{print $2}' | xargs git diff --color -- " | awk '{print $2}' | xargs git add;
+}
