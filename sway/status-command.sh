b071af8677cf898918ae48e939f196fe644ec329
Eyrim <marfxedbot@gmail.com>
2025-05-29 23:34:31 +0100

    Show whether batt is charging or not in status

--- sway/status-command.sh
+++ sway/status-command.sh
@@ -1,10 +1,10 @@
 #!/bin/zsh
 
 date_time=$(date +'%Y-%m-%d %X');
-batt_percentage=$(acpi | grep -Eo "(Discharging|Charging),\s[0-9]{1,3}%" | tr -d "DiscCharging, ");
+batt_percentage=$(acpi | grep -Eo "(Discharging|Charging),\s[0-9]{1,3}%" | tr -d ",");
 ssid=$(iwconfig wlan0 | head -n 1 | awk '{print $4}' | grep -oE \".+\" | tr -d '"')
 
-echo "$ssid $date_time $batt_percentage";
+echo "$ssid : $date_time : $batt_percentage";
 
 return 0;
 
