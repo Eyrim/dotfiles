ad21f6acaf89342ae4e0039a56e5c676fa9397f9
Eyrim <marfxedbot@gmail.com>
2025-06-10 08:06:36 +0100

    Config gitsigns and stuff

--- nvim/after/plugin/git.lua
+++ nvim/after/plugin/git.lua
@@ -2,7 +2,8 @@ local neogit = require('neogit')
 
 neogit.setup({
     graph_style = "unicode",
-    process_spinner = true
+    process_spinner = true,
+    disable_insert_on_commit = true,
 })
 
 -- Open neogit, (Should override the one provided by the plugin)
@@ -15,4 +16,19 @@ vim.api.nvim_create_user_command(
         nargs = 0
     }
 )
+
+require('gitsigns').setup({
+    current_line_blame = true,
+    current_line_blame_opts = {
+        virt_text = true,
+        virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+        delay = 500,
+        ignore_whitespace = false,
+        virt_text_priority = 100,
+        use_focus = true,
+    },
+})
+
+-- The HL for the current line blame text
+vim.api.nvim_set_hl(0, 'GitSignsCurrentLineBlame', { fg = "#a6a4a4", bold = false })
 
