d9370728aa8c06d002383a54eec2adc9040d21dd
Eyrim <marfxedbot@gmail.com>
2025-06-10 08:06:16 +0100

    Add flog

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -3,8 +3,19 @@ return {
         "NeogitOrg/neogit",
         -- also require's telescope :)
         dependencies = {
-            "nvim-lua/plenary.nvim", -- required
+            "nvim-lua/plenary.nvim",  -- required
             "sindrets/diffview.nvim", -- optional - Diff integration
         },
-    }
+    },
+    {
+        "rbong/vim-flog",
+        lazy = true,
+        cmd = { "Flog", "Flogsplit", "Floggit" },
+        dependencies = {
+            "tpope/vim-fugitive",
+        },
+    },
+    {
+        "lewis6991/gitsigns.nvim",
+    },
 }
