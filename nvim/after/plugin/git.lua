68743e64d6628e4a0c82a2a85ad18a4f77816746
Eyrim <marfxedbot@gmail.com>
2025-06-10 07:01:28 +0100

    Override neogit user cmd and provide some settings

--- nvim/after/plugin/git.lua
+++ nvim/after/plugin/git.lua
@@ -1,2 +1,18 @@
-require('neogit')
+local neogit = require('neogit')
+
+neogit.setup({
+    graph_style = "unicode",
+    process_spinner = true
+})
+
+-- Open neogit, (Should override the one provided by the plugin)
+vim.api.nvim_create_user_command(
+    'Neogit',
+    function()
+        neogit.open({ kind = "floating" })
+    end,
+    {
+        nargs = 0
+    }
+)
 
