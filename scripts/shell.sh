dcd5307961eaaa290c87237f2938e2fd3160cc60
Eyrim <marfxedbot@gmail.com>
2025-06-10 06:52:39 +0100

    Make mkcd a function instead of an alias because we need args

--- scripts/shell.sh
+++ scripts/shell.sh
@@ -1,7 +1,10 @@
 #!/bin/zsh
 
 # Make a dir and cd into it in one go
-alias mkcd='mkdir $1; cd $1;';
+function mkcd () {
+    mkdir $1;
+    cd $1;
+}
 # Source zshrc without having to specify the path each time
 # Nice when I'm somewhere random and can't be bothered to cd
 alias src-zsh='source ~/.zshrc';
