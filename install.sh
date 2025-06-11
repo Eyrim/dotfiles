fa62611d9ec5aa8e5106ccd1d1e03fd9edc90fa4
Eyrim <marfxedbot@gmail.com>
2025-06-11 23:56:54 +0100

    Setup assets like my background with a loop

--- install.sh
+++ install.sh
@@ -1,12 +1,33 @@
 #!/bin/zsh
 
+config="${HOME}/.config";
+
 function setup-symlinks() {
     # Symlink zshrc from config dir to ~
-    ln -s "${HOME}/.config/.zshrc" "${HOME}/.zshrc";
+    ln -s "${config}/.zshrc" "${HOME}/.zshrc";
 
     # Symlink scripts to proper place
-    ln -s "${HOME}/.config/scripts/" "~/scripts";
+    ln -s "${config}/scripts/" "${HOME}/scripts";
 }
 
+# Setup the assets used by the system, this doesn't include things like css files for wofi
+# This is more stuff like my background
+# 
+# Note: This works by just moving every file in the assets dir here to the documents dir as is, don't expect anything fancy
+function setup-assets() {
+    for f in $(find "${config}/assets");
+    do
+        # This will break if you have forward slashes in your file name, but if you do that you don't deserve a nice wallpaper
+        name=$(echo "${f}" | awk -F '/' '{print $(NF)}'); # Get the last column
+        # TODO: Track down the find flag that doesn't print the assets dir here, this is a lazy hack to get rid of that behaviour manually
+        if [[ "$name" != "assets" ]];
+        then
+            echo "${name}";
+        fi
+        ln -s "$f" "${HOME}/Documents/${name}";
+    done
+}
+
+setup-assets;
 setup-symlinks;
 
