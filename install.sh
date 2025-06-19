2c8f883de02b797fe7428838191696a9e9255ad5
Eyrim <marfxedbot@gmail.com>
2025-06-19 23:12:23 +0100

    Don't do a stupid complicated loop, just link the assets dir
    Remove comment explaining the weird old asset linking

--- install.sh
+++ install.sh
@@ -12,19 +12,10 @@ function setup-symlinks() {
 
 # Setup the assets used by the system, this doesn't include things like css files for wofi
 # This is more stuff like my background
-# 
-# Note: This works by just moving every file in the assets dir here to the documents dir as is, don't expect anything fancy
 function setup-assets() {
-    for f in $(find "${config}/assets");
-    do
-        # This will break if you have forward slashes in your file name, but if you do that you don't deserve a nice wallpaper
-        name=$(echo "${f}" | awk -F '/' '{print $(NF)}'); # Get the last column
-        # TODO: Track down the find flag that doesn't print the assets dir here, this is a lazy hack to get rid of that behaviour manually
-        if [[ "$name" != "assets" ]];
-        then
-            ln -s "$f" "${HOME}/Documents/${name}";
-        fi
-    done
+    asset_dir="assets";
+
+    ln -s "${config}/${asset_dir}" "${HOME}/Documents/${asset_dir}";
 }
 
 setup-assets;
