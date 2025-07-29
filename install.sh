c2ce57f7fc75f8b1738a01815538740cb1c28152
Eyrim <marfxedbot@gmail.com>
2025-07-29 01:28:33 +0100

    Fix wrong use of XDG_CONFIG_DIR

--- install.sh
+++ install.sh
@@ -1,11 +1,27 @@
 #!/bin/zsh
 
+set -e;
+
+# Returns the first argument passed to it if it is a string with a value (see, -z).
+# If the first arg does not have a value, then returns the second arg, regardless of its value.
+function arg-or() {
+    if [[ -z "$1" ]]; then
+        echo "$2";
+        return 0;
+    fi
+
+    echo "$1";
+    return 0;
+}
+
+CONFIG_DIR=$(arg-or "${XDG_CONFIG_DIR}" "${HOME}/.config");
+
 function setup-symlinks() {
     # Symlink zshrc from config dir to ~
-    ln -s "${XDG_CONFIG_DIR}/.zshrc" "${XDG_CONFIG_DIR}/.zshrc";
+    ln -s "${CONFIG_DIR}/.zshrc" "${HOME}/.zshrc";
 
     # Symlink scripts to proper place
-    ln -s "${XDG_CONFIG_DIR}/scripts/" "${XDG_CONFIG_DIR}/scripts";
+    ln -s "${CONFIG_DIR}/scripts/" "${HOME}/scripts";
 }
 
 # Setup the assets used by the system, this doesn't include things like css files for wofi
@@ -13,9 +29,8 @@ function setup-symlinks() {
 function setup-assets() {
     asset_dir="assets";
 
-    ln -s "${XDG_CONFIG_DIR}/${asset_dir}" "${HOME}/Documents/${asset_dir}";
+    ln -s "${CONFIG_DIR}/${asset_dir}" "${HOME}/Documents/${asset_dir}";
 }
 
 setup-assets;
 setup-symlinks;
-
