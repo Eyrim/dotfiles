e63265029cd13ef95be3ee14c5cecaaa951e1d13
Eyrim <marfxedbot@gmail.com>
2025-10-14 01:46:54 +0100

    Add lazydev

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -20,5 +20,18 @@ return {
     },
     {
         "williamboman/mason.nvim"
+    },
+    {
+        "folke/lazydev.nvim",
+        dependencies = {
+            'DrKJeff16/wezterm-types',
+            lazy = true,
+            version = false,
+        },
+        opts = {
+            library = {
+                { path = 'wezterm-types', mods = { 'wezterm' } },
+            }
+        },
     }
 }
