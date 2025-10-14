450eb52c0282a4db2c7dfd3ab9d7e77f5fa83326
Eyrim <marfxedbot@gmail.com>
2025-10-14 01:46:34 +0100

    move show rust error bind to ftplugin

--- nvim/after/plugin/rustacean.lua
+++
@@ -1,5 +0,0 @@
--- note that on my arch machine rust-analyzer is installed with pacman, not mason
-
--- :RustLsp renderDiagnostic
-vim.keymap.set("n", "<leader>se", "<CMD>RustLsp renderDiagnostic current<Cr>")
-
