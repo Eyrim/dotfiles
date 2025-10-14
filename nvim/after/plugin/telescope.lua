ceddda9b7897421c509f8c56d7b1a9e8e11cd5d0
Eyrim <marfxedbot@gmail.com>
2025-10-14 01:46:49 +0100

    Add file browser to telescope
    Add file browser to telescope

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,5 +1,14 @@
+local telescope = require("telescope")
 local builtin = require('telescope.builtin')
 
 vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>ht', builtin.help_tags, { desc = 'Telescope help tags' })
+
+telescope
+    .setup({
+        "nvim-telescope/telescope-file-browser.nvim",
+        dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
+    })
+telescope
+    .load_extension("file_browser")
